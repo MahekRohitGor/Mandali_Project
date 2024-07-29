f9ebf92c189a3c029c2b69e06940048e7e51ede0
Mahek Rohit Gor <1mahekgor1@gmail.com>
2024-07-29 21:06:08 +0530

    modified

--- pages/data_display.py
+++ pages/data_display.py
@@ -16,7 +16,7 @@ data_sheet = client.open("Mandali").worksheet("Data")
 def get_data(month, year, user_id):
     data = data_sheet.get_all_records()
     df = pd.DataFrame(data)
-    st.write(user_id)
+    # st.write(user_id)
     # print(df)
 
     df['Month'] = df['Month'].astype(str)
@@ -31,36 +31,96 @@ def get_data(month, year, user_id):
         return filtered_data
 
 # Streamlit app
-st.title("Member Data Display")
+st.title("સભ્ય ડેટા ડિસ્પ્લે")
+st.subheader("શ્રી કચ્છ કેળિણી ખાતાના કર્મચારીઓની શરાફી સહકારી મંડળી લી.")
 
 # Check if the user is logged in
 if 'logged_in' not in st.session_state or not st.session_state.logged_in:
-    st.warning("Please log in first.")
+    st.warning("કૃપા કરીને પહેલા લોગ ઇન કરો.")
     st.stop()
 
 # User is logged in, display data
-st.subheader("Select Month and Year")
+st.subheader("મહિનો અને વર્ષ પસંદ કરો")
 month = st.selectbox("Month", ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"])
 year = st.selectbox("Year", [2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2024, 2035, 2036, 2037, 2038, 2039, 2040, 2041, 2042, 2043, 2044, 2045, 2046, 2047, 2048, 2049, 2050])
 
-if st.button("Display Data"):
+if st.button("ડિસ્પ્લે ડેટા"):
     user_id = st.session_state['user_id']
     data = get_data(month, year, user_id)
     if not data.empty:
         for index, row in data.iterrows():
-            st.write(f"**Sabhasad No.:** {row['Sabhasad No.']}")
-            st.write(f"**Name:** {row['Name']}")
-            st.write(f"**School Name:** {row['School Name']}")
-            st.write(f"**Group Name:** {row['Group name']}")
-            st.write(f"**Share Amount:** {row['Share Amount']}")
-            st.write(f"**Savings:** {row['Savings']}")
-            st.write(f"**Loan Amount:** {row['Loan Amount']}")
-            st.write(f"**Installment:** {row['Installment']}")
-            st.write(f"**Interest:** {row['Intrest']}")
-            st.write(f"**Compulsory Saving:** {row['Compulsory Saving']}")
-            st.write(f"**Total:** {row['Total']}")
-            st.write(f"**Year:** {row['Year']}")
-            st.write(f"**Month:** {row['Month']}")
+            st.markdown(
+            f"""
+            <table style="border: 1px solid black; width: 100%;">
+                <tr>
+                    <td style="border: 1px solid black;"><strong>સભાસદ નં.:</strong></td>
+                    <td style="border: 1px solid black;">{row['Sabhasad No.']}</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid black;"><strong>નામ:</strong></td>
+                    <td style="border: 1px solid black;">{row['Name']}</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid black;"><strong>શાળાનું નામ:</strong></td>
+                    <td style="border: 1px solid black;">{row['School Name']}</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid black;"><strong>ગ્રુપ નામ:</strong></td>
+                    <td style="border: 1px solid black;">{row['Group name']}</td>
+                </tr>
+            </table>
+            """, unsafe_allow_html=True)
+
+            st.write(f"આપની માસ : **{row['Month']}** - **{row['Year']}** સુધી જમા થયેલ શેરભંડોળ , ફરજીયાત બચતની રકમ તથા મંડળીમાંથી લીધેલ લોનની બાકી રહેલ રકમ, આ **{row['Month']}** - **{row['Year']}** મહિનામાં જે કપાત કરિાની થાય છે તે મુદ્દલ,વ્યાજ અને ફરજીયાત બચત રકમનિ કપાત વિગત નીચે મુજબ છે.")
+        
+            # col1, col2 = st.columns(2)
+        
+            # with col1:
+            st.write("મંડળીમા આપની જમા થયેલ રકમનિ વિગત")
+            st.markdown(
+                f"""
+                <table style="border: 1px solid black; width: 100%;">
+                    <tr>
+                        <td style="border: 1px solid black;"><strong>શેરની રકમ:</strong></td>
+                        <td style="border: 1px solid black;">₹{row['Share Amount']}</td>
+                    </tr>
+                    <tr>
+                        <td style="border: 1px solid black;"><strong>બચત:</strong></td>
+                        <td style="border: 1px solid black;">₹{row['Savings']}</td>
+                    </tr>
+                </table>
+                """, unsafe_allow_html=True)
+        
+            # with col2:
+            st.write(f"**{row['Month']}** - **{row['Year']}** મહિનામાં થનાર કપાતની વિગત")
+            st.markdown(
+                    f"""
+                    <table style="border: 1px solid black; width: 100%;">
+                        <tr>
+                        <td style="border: 1px solid black;"><strong>લોનની રકમ:</strong></td>
+                        <td style="border: 1px solid black;">₹{row['Loan Amount']}</td>
+                        </tr>
+                        <tr>
+                        <td style="border: 1px solid black;"><strong>હપ્તો:</strong></td>
+                        <td style="border: 1px solid black;">₹{row['Installment']}</td>
+                        </tr>
+                    <tr>
+                        <td style="border: 1px solid black;"><strong>વ્યાજ:</strong></td>
+                        <td style="border: 1px solid black;">₹{row['Intrest']}</td>
+                    </tr>
+                    <tr>
+                        <td style="border: 1px solid black;"><strong>ફરજિયાત બચત:</strong></td>
+                        <td style="border: 1px solid black;">₹{row['Compulsory Saving']}</td>
+                    </tr>
+                    <tr>
+                        <td style="border: 1px solid black;"><strong>કુલ:</strong></td>
+                        <td style="border: 1px solid black;">₹{row['Total']}</td>
+                    </tr>
+                </table>
+                """, unsafe_allow_html=True)
+        
+            st.write(f"**વર્ષ:** {row['Year']}")
+            st.write(f"**માસ:** {row['Month']}")
             st.write("---")
         
         # Visualization example
@@ -73,7 +133,7 @@ if st.button("Display Data"):
         total_amount = data["Total"].sum()
 
         pie_chart_data = pd.DataFrame({
-            "Category": ["Loan Amount", "Installment", "Interest", "Compulsory Saving"],
+            "Category": ["લોનની રકમ", "હપ્તો", "વ્યાજ", "ફરજિયાત બચત"],
             "Amount": [loan_amount, installment, interest, compulsory_saving]
         })
 
@@ -84,9 +144,9 @@ if st.button("Display Data"):
         # Display total amount
         st.write(f"**Total Amount:** {total_amount}")
     else:
-        st.warning("No data found for the selected month and year")
-logout_button = st.button("Logout")
+        st.warning("પસંદ કરેલ મહિના અને વર્ષ માટે કોઈ ડેટા મળ્યો નથી")
+logout_button = st.button("લૉગ આઉટ")
 if logout_button:
     del st.session_state['logged_in']
-    st.success("You have been logged out.")
+    st.success("તમે લૉગ આઉટ થઈ ગયા છો.")
     st.stop()
